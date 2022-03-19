b71ec1abbb6375639708f74941d1eb5b3af4051d
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2022-03-19 13:24:41 +0530

    Added Unit tests and restructured the project

---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatternUsage;
+using DesignPatternUsage.Discounts;
+
+var cartItems = new List<Item>()
+{
+    new Item("Product 1", 200, 3),
+    new Item("Product 2", 300),
+    new Item("Product 3", 500, 3)
+};
+
+// Add discount to items
+cartItems[0].Discounts.Add(new BuyTwoGetOneFreeDiscountRule());
+
+PriceCalculator calculator = new();
+var total = calculator.CalculatePrice(cartItems);
+
+Console.WriteLine("Total Amount : " + total);
+
+Console.ReadLine();
---
+++ tests/PriceCalculatorIntegrationTests.cs
@@ -0,0 +1,41 @@
+using DesignPatternUsage.Discounts;
+using System.Collections.Generic;
+using Xunit;
+namespace DesignPatternUsage.Tests;
+
+public class PriceCalculatorIntegrationTests
+{
+    [Fact]
+    public void ReturnsCorrectTotal_WhenItemsAddedWithoutDiscount()
+    {
+        var cartItems = new List<Item>()
+        {
+            new Item("Product 1", 100, 3),
+            new Item("Product 2", 100),
+            new Item("Product 3", 100, 3)
+        };
+
+        PriceCalculator priceCalculator = new();
+        var price = priceCalculator.CalculatePrice(cartItems);
+
+        Assert.Equal(700, price);
+    }
+
+    [Fact]
+    public void WhenB2G1Added_CartAddsExtraItem_WithoutIncreaseInPrice()
+    {
+        var cartItems = new List<Item>()
+        {
+            new Item("Product 1", 100, 3),
+            new Item("Product 2", 100),
+            new Item("Product 3", 100, 3)
+        };
+
+        cartItems[0].Discounts.Add(new BuyTwoGetOneFreeDiscountRule());
+
+        PriceCalculator priceCalculator = new();
+        _ = priceCalculator.CalculatePrice(cartItems);
+
+        Assert.Equal(4, cartItems[0].Quantity);
+    }
+}
